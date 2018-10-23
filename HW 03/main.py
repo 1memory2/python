bd1a3fd65aba6e6d066957a637b917e5e454cb67
1memory2 <674754010@qq.com>
2018-10-23 20:04:45 +0800

    图形面积和

--- HW 03/main.py
+++ HW 03/main.py
@@ -1,1 +1,5 @@
-
+shapes = [Ellipse(10,20), Square(15), Circle(5), Rectangle(20,15), Circle(5), Square(15), Ellipse(10,20)] 
+areas = [x.area for x in shapes]
+print('areas: ', areas)
+total_area = sum(areas)
+print('total_area: ', total_area)
